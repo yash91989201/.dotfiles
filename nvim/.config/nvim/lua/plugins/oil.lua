6ceeaaee3ea8dadf44ef047cdc3505bc14530bf6
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-04-29 10:03:02 +0530

    remove oil nvim extra keymap

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -24,19 +24,12 @@ return {
       view_options = {
         show_hidden = true,
         is_always_hidden = function(name, _)
-          local folder_skip = { "dev-tools.locks", "dune.lock", "_build" }
+          local folder_skip = { ".next", "node_modules", "dev-tools.locks", "dune.lock", "_build" }
           return vim.tbl_contains(folder_skip, name)
         end,
       },
     })
 
     vim.keymap.set("n", "<leader>fo", "<CMD>Oil<CR>", { desc = "Open parent directory in current window" })
-
-    vim.keymap.set(
-      "n",
-      "<leader>fO",
-      require("oil").toggle_float,
-      { desc = "Open parent directory in floating window" }
-    )
   end,
 }
