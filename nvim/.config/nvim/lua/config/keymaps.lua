98f88615dbcb16d60084b935686821a713615fe9
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-05-04 23:32:26 +0530

    fix: update keymap, remove un-necessary ones

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,18 +2,18 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
-keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 
 keymap.set("n", "x", '"_x')
 
--- Increment/decrement
-keymap.set("n", "+", "<C-a>")
-keymap.set("n", "-", "<C-x>")
+keymap.set("n", "+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "-", "<C-x>", { desc = "Decrement number" })
 
--- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
+keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
 
 keymap.set("n", "<leader>mq", ":wqa!<Return>", { desc = "Save and quit all", noremap = true, silent = true })
 keymap.set("n", "<leader>ms", ":wa!<Return>", { desc = "Save all", noremap = true, silent = true })
+
+keymap.set("n", "<leader>cn", function()
+  require("nvim-navbuddy").open()
+end, { desc = "Navbuddy Open" })
