1062ba5f587f7349253d53ff89d7c95a34c9b24e
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-05-08 21:53:30 +0530

    fix: remove navic and navbuddy because aerial is configured as required

--- nvim/.config/nvim/lua/plugins/aerial.lua
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -3,9 +3,18 @@ return {
   event = "LspAttach",
   opts = {
     layout = {
-      max_width = 50,
-      min_width = 25,
+      max_width = 60,
+      min_width = 40,
+      resize_to_content = true,
     },
     lazy_load = true,
+    backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
+    filter_kind = false,
+    show_guides = true,
+    highlight_on_hover = true,
+    manage_folds = true,
+    link_folds_to_tree = true,
+    link_tree_to_folds = true,
+    autojump = true,
   },
 }
