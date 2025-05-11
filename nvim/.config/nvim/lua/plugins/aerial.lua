c6ca2e525d06d22bdc116b344a3f85be04d29984
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-05-11 08:56:10 +0530

    update aerial opts

--- nvim/.config/nvim/lua/plugins/aerial.lua
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -1,14 +1,12 @@
 return {
   "stevearc/aerial.nvim",
-  event = "LspAttach",
   opts = {
     layout = {
-      max_width = 60,
-      min_width = 40,
+      max_width = 50,
+      min_width = 35,
       resize_to_content = true,
     },
     lazy_load = true,
-    backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
     filter_kind = false,
     show_guides = true,
     highlight_on_hover = true,
