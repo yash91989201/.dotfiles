6dd3be1fa660ece629865b741a27fb83bdc28812
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-05-12 18:58:01 +0530

    fix: remove img-clip plugin

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -31,20 +31,6 @@ return {
     "echasnovski/mini.icons",
     "folke/snacks.nvim",
     {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-        },
-      },
-    },
-    {
       -- Make sure to set this up properly if you have lazy=true
       "MeanderingProgrammer/render-markdown.nvim",
       opts = {
