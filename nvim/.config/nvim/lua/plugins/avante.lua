dbca375b2eccd0d78d4e5207155c9f3a5f528921
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-05-19 20:23:34 +0530

    fix: turn off auto suggestions in avante

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -22,7 +22,7 @@ return {
   opts = {
     provider = "gemini",
     behaviour = {
-      auto_suggestions = true,
+      auto_suggestions = false,
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
       auto_apply_diff_after_generation = false,
