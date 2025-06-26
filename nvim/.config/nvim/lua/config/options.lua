3f151f518038f81924dc5a81c418173632b881bd
Yashraj Jaiswal <yashraj6370@gmail.com>
2025-06-26 18:14:47 +0530

    fix: snacks file explorer opening sub-dir instead of cwd

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -8,6 +8,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.title = true
+vim.g.root_spec = { "cwd" }
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.hlsearch = true
